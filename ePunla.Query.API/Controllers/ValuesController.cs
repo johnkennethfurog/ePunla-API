404ca11931466b1ec1f43e8c836c3bdf010c63d8
furog <johnkennethfurog@gmail.com>
2021-05-09 00:33:18 +0800

    setup deployment for command

--- ePunla.Query.API/Controllers/ValuesController.cs
+++ ePunla.Query.API/Controllers/ValuesController.cs
@@ -11,36 +11,11 @@ namespace ePunla.Query.API.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET api/values/5
         [HttpGet("{key}")]
         public string Env(string key)
         {
             return Environment.GetEnvironmentVariable(key);
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
