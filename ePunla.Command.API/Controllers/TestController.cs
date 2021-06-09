1df3ca984524245fca35f64057dd632ffef04c41
furog <johnkennethfurog@gmail.com>
2021-06-09 12:19:21 +0800

    test modification

--- ePunla.Command.API/Controllers/TestController.cs
+++ ePunla.Command.API/Controllers/TestController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,8 +8,7 @@ namespace ePunla.Command.API.Controllers
     [Route("api/[controller]")]
     public class TestController : Controller
     {
-        // GET api/values/5
-        [HttpGet("{key}")]
+        [HttpGet("checkEnv/{key}")]
         public string Env(string key)
         {
             // some changes
